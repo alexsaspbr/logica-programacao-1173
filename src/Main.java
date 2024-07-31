381777552a2ce85dee2abb05dd0b7a22bd5803a1
Ana Laura Marques <lauramarques181@gmail.com>
2024-07-31 20:56:19 -0300

    add meu nome -laura

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         String [] nomes = {
                 "Alex"
-                ,"Cassio"
+                ,"Cassio","Laura"
         };
         for(String nome : nomes)
             System.out.printf("Ola %s! \n", nome);
