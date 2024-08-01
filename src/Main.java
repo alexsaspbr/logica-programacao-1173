258cfa90f2be92a74d349275aa408edb15f884d9
Marina Guimarães <mama_guima04@hotmail.com>
2024-07-31 21:51:46 -0300

    Adicionando o sobrenome

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         String [] nomes = {
-                "Alex"
-                ,"Cassio"
-                ,"Marina"
+                "Alex Araujo"
+                ,"Cassio Kendi"
+                ,"Karine"
+                ,"Larissa"
+                ,"Marina Guimarães"
         };
         for(String nome : nomes)
             System.out.printf("Ola %s! \n", nome);
