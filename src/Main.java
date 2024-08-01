8adcb8ab13d9a3b04275dcfff2552db7ff3d50ef
Alex Araujo <alexsaspbr@gmail.com>
2024-07-31 21:32:27 -0300

    adicionando sobrenome do cassio kendi

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         String [] nomes = {
                 "Alex Araujo"
-                ,"Cassio"
+                ,"Cassio Kendi"
                 ,"Karine"
                 ,"Larissa"
         };
