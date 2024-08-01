b2cef1c97567ea341352f22705183ba00fb51512
Lucas Kaique <lucaskaique131@gmail.com>
2024-07-31 21:31:44 -0300

    lucas kaique

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         String [] nomes = {
                 "Alex"
+                ,"Lucas Kaique"
                 ,"Cassio"
                 ,"Arthur"
         };
