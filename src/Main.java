024c33848fd1a375a45344a2bd850dd8e260812c
Alana Sampaio <alana.sampaio2000@gmail.com>
2024-08-02 18:10:51 -0300

    Adicionando meu nome

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         String [] nomes = {
-                "Alex"
-                ,"Cassio"
-                ,"Arthur"
+                "Alex Araujo"
+                ,"Cassio Kendi"
+                ,"Karine"
+                ,"Larissa"
+                ,"Alana Sampaio"
         };
         for(String nome : nomes)
             System.out.printf("Ola %s! \n", nome);
