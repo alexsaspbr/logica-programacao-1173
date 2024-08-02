4ec1eee8b09b03c886eec32a43818c7e6c944670
Beatriz <bsciamarelli@gmail.com>
2024-08-01 21:28:01 -0300

    feat: add my name on the nomes array

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
                 ,"Cassio Kendi"
                 ,"Karine"
                 ,"Larissa"
+                ,"Beatriz"
         };
         for(String nome : nomes)
             System.out.printf("Ola %s! \n", nome);
