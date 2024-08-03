7126d732ad71f3fc5ea8c3b521042bcea58b08e4
Alex Araujo <alexsaspbr@gmail.com>
2024-08-03 10:54:33 -0300

    aula 02 - tipos, variaveis, e constante; operadores; entradas e saidas

---
+++ README.md
@@ -0,0 +1,16 @@
+Exercicio 1
+
+Crie um programa que pergunte qual a altura em metros e peso em Kg desta pessoa e retorne o IMC e a classificação deste IMC.
+>  IMC = peso / altura x altura;
+
+Como referência para para a classificação do IMC utilizar a tabela do link [Tabela IMC - Wikipedia](https://pt.wikipedia.org/wiki/%C3%8Dndice_de_massa_corporal#Tabela_de_IMC).
+
+Exemplo:
+
+```language
+Informe sua altura em metros?
+1,9
+Informe seu peso em Kg?
+72,2
+O valor do seu IMC é 20 (considerado Peso normal)
+```
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,27 @@
+import java.math.BigDecimal;
+
 public class Main {
 
     public static void main(String[] args) {
-        String [] nomes = {
-                "Alex"
-                ,"Cassio"
-        };
-        for(String nome : nomes)
-            System.out.printf("Ola %s! \n", nome);
+
+        Double salario = Double.parseDouble(args[0]);
+        Util.inss(salario);
+
+    }
+
+
+
+
+}
+
+class Util {
+
+    public static Double inss(Double salario) {
+
+        return (salario <= 1212) ? salario * 0.075 :
+                      (salario <= 2427.35) ? salario * 0.09 :
+                      (salario <= 3641.03) ? salario * 0.12 : salario * 0.14;
+
     }
 
 }
