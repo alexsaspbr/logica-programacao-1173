a6ece5630c017d6cae22dbf0cea56745ef7e6732
Alex Araujo <alexsaspbr@gmail.com>
2024-08-07 20:55:07 -0300

    aula4 arrays e matrizes

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,53 @@ import ada.modelos.PessoaFisica;
 import ada.utilitario.Util;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Pessoa pessoa = new Pessoa("Alex", "alex@ada.tech");
-        PessoaFisica pessoaFisica = new PessoaFisica("Alex", "alex@ada.tech");
+        /*String nome = "Alex";
+        int [] numeros =  {1, 2, 3};
+        Integer [] numeros2 =  new Integer [10];
+        System.out.println(nome.charAt(2));
+        System.out.println(nome.toCharArray());
+        System.out.println(Arrays.toString(numeros));
+        numeros2[2] = 12;
+        System.out.println(Arrays.toString(numeros2));*/
 
-        pessoa.getNome();
-        pessoaFisica.getNome();
-/*        pessoa.metodoDefault();
-        pessoa.ninguemVaiAcessarSomentePessoa();
-        pessoaFisica.getEmail();*/
+        int [][] matriz = new int[2][2];
+        int [][] matriz2 = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
 
-        //Util.SELIC = 2d;
+        printMatriz(matriz);
+        //printMatriz(matriz2);
 
-        //System.out.println(Util.leituraEscrita("Qual o seu nome?"));
-        Util.escrita(Util.leitura("Qual o seu nome?"));
+        insertMatriz(matriz);
+        printMatriz(matriz);
+    }
 
+    private static void insertMatriz(int[][] matriz) {
+        Scanner input = new Scanner(System.in);
+        for(int i = 0; i < matriz.length; i++) {
+            for(int j = 0; j < matriz[i].length; j++) {
+                System.out.printf("Digite o numero para a linha %d e coluna %d \n", (i+1), (j+1));
+                matriz[i][j] = Integer.parseInt(input.nextLine());;
+            }
+        }
+    }
+
+    private static void printMatriz(int[][] matriz) {
+        for(int i = 0; i < matriz.length; i++) {
+            for(int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
 
